c3b780e6f3c47b6f052b4dac35edcee767ffa7c0
Akkenton <akkenton.msk@gmail.com>
2022-11-23 22:11:32 +0300

    Добавили операнд $ для идентификации переменных в writeline

--- task001/Program.cs
+++ task001/Program.cs
@@ -2,4 +2,4 @@
 Console.Write("Enter: ");
 int number = int.Parse(Console.ReadLine());
 double square = Math.Pow(number, 2);
-Console.WriteLine("Квадрат числа " + number + " равен " + square);
+Console.WriteLine($"Квадрат числа {number} равен {square}");
