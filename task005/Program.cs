998207324fef4f1dfd4bde7e053c65d72ea4507b
Akkenton <akkenton.msk@gmail.com>
2022-11-23 23:12:13 +0300

    Решена задача по выделению последней цифре в трёхзначных и более числах

--- task005/Program.cs
+++ task005/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Enter: ");
+int num = int.Parse(Console.ReadLine());
+int lastN;
+if (num > -100 && num < 100)
+{
+    Console.WriteLine($"Число {num} меньше трехзначного!");
+}
+else
+{
+    lastN = num % 10;
+    Console.WriteLine($"Последняя цифра из числа {num} является {lastN}");
+}
