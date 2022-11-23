4b83ff9536f0c8ebf24e4d2764a1d1bdcae44e9c
Akkenton <akkenton.msk@gmail.com>
2022-11-23 23:03:22 +0300

    Решение задачи по периоду от отрицательного до положительного

--- task004/Program.cs
+++ task004/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите число периода: ");
+int N = int.Parse(Console.ReadLine());
+int otrN = -N;
+Console.WriteLine(otrN);
+
+if (N == 0)
+{
+    Console.WriteLine("Период равен 0");
+}
+
+if (N > 0)
+{
+    while (otrN <= N)
+    {
+        Console.Write(otrN + " ");
+        otrN++;
+    }
+}
+
+if (N < 0)
+{
+    while (N <= otrN)
+    {
+        Console.Write(N + " ");
+        N++;
+    }
+}
