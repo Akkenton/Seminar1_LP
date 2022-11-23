3ef129e392a4043dae635e157b04aff285d0410b
Akkenton <akkenton.msk@gmail.com>
2022-11-23 22:31:24 +0300

    Решение задачи по определению квадрата числа

--- task002/Program.cs
+++ task002/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (Math.Pow(number2, 2) == number1)
+{
+    Console.WriteLine("TRUE!");
+}
+else
+{
+    Console.WriteLine("FALSE!");
+}
