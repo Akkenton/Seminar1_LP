a8637d9e832b46f51f2e48b284e445c77b14655d
Akkenton <akkenton.msk@gmail.com>
2022-11-23 22:05:56 +0300

    Возведение в степень

--- task001/Program.cs
+++ task001/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Enter: ");
+int number = int.Parse(Console.ReadLine());
+double square = Math.Pow(number, 2);
+Console.WriteLine(square);
